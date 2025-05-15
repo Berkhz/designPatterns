f5c6c976580d4df3b78d30c9ab15217c6b224c2a
Kauan Bertalha <kauan.hb2004@gmail.com>
2025-05-15 20:14:48 -0300

    up

---
+++ SOA/ibge-nomes/src/main/java/com/example/ibgenomes/IbgeNomesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ibgenomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbgeNomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbgeNomesApplication.class, args);
+	}
+
+}
---
+++ SOA/ibge-nomes/src/test/java/com/example/ibgenomes/IbgeNomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibgenomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbgeNomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
